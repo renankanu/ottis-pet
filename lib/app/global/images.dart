3eb04f3dda56ac8a7bb6f9c014a5c733ffab7ace
renankanu <renankanu@gmail.com>
2021-03-19 20:18:24 -0300

    add image for welcomescreen

--- lib/app/global/images.dart
+++ lib/app/global/images.dart
@@ -1,1 +1,2 @@
 const String kLogoSplash = 'assets/images/ottis.png';
+const String kBackgroundLogin = 'assets/images/login_screen.png';
--- lib/app/modules/welcome/view/welcome_view.dart
+++ lib/app/modules/welcome/view/welcome_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ottis_pet/app/global/images.dart';
 import 'package:ottis_pet/app/global/widgets/rounded_button.dart';
 import 'package:ottis_pet/app/modules/welcome/controllers/welcome_controller.dart';
 
@@ -7,36 +8,43 @@ class WelcomeView extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Ottis Pet",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: Get.height * 0.05),
-            Image.network(
-              'https://picsum.photos/200',
-              height: Get.height * 0.45,
-            ),
-            SizedBox(height: Get.height * 0.05),
-            RoundedButton(
-              text: "LOGIN",
-              press: () {
-                Get.offAndToNamed('/login');
-              },
-              color: Get.theme.accentColor,
-            ),
-            RoundedButton(
-              text: "REGISTRAR",
-              color: Get.theme.primaryColorLight,
-              textColor: Colors.black,
-              press: () {
-                Get.offAndToNamed('/signup');
-              },
-            ),
-          ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(kBackgroundLogin), fit: BoxFit.fitHeight)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Ottis Pet",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: Get.height * 0.05),
+              Image.network(
+                'https://picsum.photos/200',
+                height: Get.height * 0.45,
+              ),
+              SizedBox(height: Get.height * 0.05),
+              RoundedButton(
+                text: "LOGIN",
+                press: () {
+                  Get.offAndToNamed('/login');
+                },
+                color: Get.theme.accentColor,
+              ),
+              RoundedButton(
+                text: "REGISTRAR",
+                color: Get.theme.primaryColorLight,
+                textColor: Colors.black,
+                press: () {
+                  Get.offAndToNamed('/signup');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
