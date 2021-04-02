73138f8230f2a55b3ff315249e4682d190861410
Renan Kanu <renankanu@gmail.com>
2021-04-02 14:47:25 -0300

    refactor - login screen

---
+++ lib/app/global/widgets/custom_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:ottis_pet/app/global/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final Function onTap;
+  final bool isLoading;
+  final Color buttonColor, textColor, loadingColor;
+  const CustomButton({
+    Key key,
+    this.label,
+    this.onTap,
+    this.isLoading = false,
+    this.buttonColor = kDodgerBlue,
+    this.textColor = kWhite,
+    this.loadingColor = kWhite,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      onTap: onTap,
+      child: Ink(
+        height: 46,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: buttonColor,
+        ),
+        child: Center(
+          child: isLoading
+              ? Container(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(loadingColor),
+                    strokeWidth: 2,
+                  ),
+                )
+              : Text(
+                  label,
+                ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:ottis_pet/app/global/images.dart';
+import 'package:ottis_pet/app/global/colors.dart';
 import 'package:ottis_pet/app/global/text_style.dart';
 import 'package:ottis_pet/app/global/widgets/custom_input_field.dart';
-import 'package:ottis_pet/app/global/widgets/rounded_button.dart';
+import 'package:ottis_pet/app/global/widgets/custom_button.dart';
 import 'package:ottis_pet/app/routes/app_routes.dart';
 
 import '../controllers/login_controller.dart';
@@ -13,51 +12,49 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Ottis Pet",
-                  style: kBaseStyle.copyWith(
-                      color: Colors.black,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w600),
-                ),
-                SizedBox(height: Get.height * 0.05),
-                Card(
-                    elevation: 5,
+      backgroundColor: kCreamCan,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Ottis Pet",
+                    style: kBaseStyle.copyWith(
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: Get.height * 0.05),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    elevation: 2,
+                    shadowColor: kBlackRock,
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 24, vertical: 12),
+                      padding: const EdgeInsets.all(24),
                       child: Column(
                         children: [
                           CustomInputField(hintText: 'Email'),
                           SizedBox(height: Get.height * 0.02),
                           CustomInputField(hintText: 'Password'),
                           SizedBox(height: Get.height * 0.05),
-                          RoundedButton(
-                            text: "LOGIN",
-                            press: () {
-                              Get.offAndToNamed(Routes.HOME);
-                            },
-                            color: Get.theme.accentColor,
-                          ),
-                          RoundedButton(
-                            text: "REGISTRAR",
-                            color: Get.theme.primaryColorLight,
-                            textColor: Colors.black,
-                            press: () {
-                              Get.offAndToNamed('/signup');
-                            },
-                          ),
+                          Text('Não tem conta? Crie uma agora!')
                         ],
                       ),
-                    )),
-              ],
+                    ),
+                  ),
+                  SizedBox(height: Get.height * 0.02),
+                  CustomButton(
+                    label: 'Login',
+                    onTap: () {},
+                  ),
+                ],
+              ),
             ),
           ),
         ),
